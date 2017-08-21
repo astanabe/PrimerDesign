fd0f053413aa2464ddb56f5a10b753e9c697bb95
Akifumi S. Tanabe <akifumi.tanabe+github@gmail.com>
2017-08-21 23:57:56 +0900

    Update installer

--- installDB.sh
+++ installDB.sh
@@ -17,12 +17,12 @@ mkdir -p ~/blastdb || exit $?
 cd ~/blastdb || exit $?
 wget -c ftp://ftp.ncbi.nih.gov/blast/db/nt.??.tar.gz || exit $?
 wget -c ftp://ftp.ncbi.nih.gov/blast/db/nt.??.tar.gz.md5 || exit $?
-md5sum -c nt.??.tar.gz.md5 || exit $?
+ls nt.??.tar.gz.md5 | xargs -L 1 -P 4 md5sum -c || exit $?
 rm nt.??.tar.gz.md5 || exit $?
 wget -c ftp://ftp.ncbi.nih.gov/blast/db/taxdb.tar.gz || exit $?
 wget -c ftp://ftp.ncbi.nih.gov/blast/db/taxdb.tar.gz.md5 || exit $?
 md5sum -c taxdb.tar.gz.md5 || exit $?
 rm taxdb.tar.gz.md5 || exit $?
-for f in *.tar.gz; do tar -xzf $f; done
+ls *.tar.gz | xargs -L 1 -P 4 tar -xzf || exit $?
 chmod 644 *.tar.gz || exit $?
 rm *.tar.gz || exit $?
