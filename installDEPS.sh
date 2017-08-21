96d219e0c0f1fe909ea6a955cf677954f1274720
Akifumi S. Tanabe <akifumi.tanabe+github@gmail.com>
2017-08-21 18:30:25 +0900

    Add installer

---
+++ installDEPS.sh
@@ -0,0 +1,1 @@
+sudo apt-get -y install gcc g++ make autoconf automake python libz-dev libbz2-dev tar gzip xz-utils unzip coreutils grep perl libdbi-perl libdbd-sqlite3-perl libwww-perl libfile-copy-recursive-perl curl libcurl4-gnutls-dev
