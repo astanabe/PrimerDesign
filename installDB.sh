fa762c9baac5c00faa761585fcf30e32fbed890c
Akifumi S. Tanabe <akifumi.tanabe+github@gmail.com>
2017-08-21 18:18:31 +0900

    Update installer

--- installDB.sh
+++ installDB.sh
@@ -1,27 +1,27 @@
 # NCBI Taxonomy
-mkdir -p ~/taxonomy
-cd ~/taxonomy
-wget -c ftp://ftp.ncbi.nih.gov/pub/taxonomy/gi_taxid_nucl.dmp.gz
-wget -c ftp://ftp.ncbi.nih.gov/pub/taxonomy/gi_taxid_nucl.dmp.gz.md5
-md5sum -c gi_taxid_nucl.dmp.gz.md5
-rm gi_taxid_nucl.dmp.gz.md5
-gzip -d gi_taxid_nucl.dmp.gz
-wget -c ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz
-wget -c ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz.md5
-tar -xzf taxdump.tar.gz
-chmod 644 taxdump.tar.gz
-rm taxdump.tar.gz
+mkdir -p ~/taxonomy || exit $?
+cd ~/taxonomy || exit $?
+wget -c ftp://ftp.ncbi.nih.gov/pub/taxonomy/gi_taxid_nucl.dmp.gz || exit $?
+wget -c ftp://ftp.ncbi.nih.gov/pub/taxonomy/gi_taxid_nucl.dmp.gz.md5 || exit $?
+md5sum -c gi_taxid_nucl.dmp.gz.md5 || exit $?
+rm gi_taxid_nucl.dmp.gz.md5 || exit $?
+gzip -d gi_taxid_nucl.dmp.gz || exit $?
+wget -c ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz || exit $?
+wget -c ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz.md5 || exit $?
+tar -xzf taxdump.tar.gz || exit $?
+chmod 644 taxdump.tar.gz || exit $?
+rm taxdump.tar.gz || exit $?
 # NCBI nt
-mkdir -p ~/blastdb
-cd ~/blastdb
-wget -c ftp://ftp.ncbi.nih.gov/blast/db/nt.??.tar.gz
-wget -c ftp://ftp.ncbi.nih.gov/blast/db/nt.??.tar.gz.md5
-md5sum -c nt.??.tar.gz.md5
-rm nt.??.tar.gz.md5
-wget -c ftp://ftp.ncbi.nih.gov/blast/db/taxdb.tar.gz
-wget -c ftp://ftp.ncbi.nih.gov/blast/db/taxdb.tar.gz.md5
-md5sum -c taxdb.tar.gz.md5
-rm taxdb.tar.gz.md5
+mkdir -p ~/blastdb || exit $?
+cd ~/blastdb || exit $?
+wget -c ftp://ftp.ncbi.nih.gov/blast/db/nt.??.tar.gz || exit $?
+wget -c ftp://ftp.ncbi.nih.gov/blast/db/nt.??.tar.gz.md5 || exit $?
+md5sum -c nt.??.tar.gz.md5 || exit $?
+rm nt.??.tar.gz.md5 || exit $?
+wget -c ftp://ftp.ncbi.nih.gov/blast/db/taxdb.tar.gz || exit $?
+wget -c ftp://ftp.ncbi.nih.gov/blast/db/taxdb.tar.gz.md5 || exit $?
+md5sum -c taxdb.tar.gz.md5 || exit $?
+rm taxdb.tar.gz.md5 || exit $?
 for f in *.tar.gz; do tar -xzf $f; done
-chmod 644 *.tar.gz
-rm *.tar.gz
+chmod 644 *.tar.gz || exit $?
+rm *.tar.gz || exit $?
--- installPROGRAMS.sh
+++ installPROGRAMS.sh
@@ -1,82 +1,82 @@
-export PREFIX=`dirname ~`/`basename ~`
+export PREFIX=`dirname ~`/`basename ~` || exit $?
 # Make directories
-mkdir -p $PREFIX/bin
-cp -R ./work $PREFIX/
+mkdir -p $PREFIX/bin || exit $?
+cp -R ./work $PREFIX/ || exit $?
 # Install ecoPCR
-tar -xzf ecoPCR-0.8.0.tar.gz
-cd ecoPCR-0.8.0/src
-make -j4
-cp ecofind ecogrep ecoisundertaxon ecoPCR $PREFIX/bin/
-cd ../tools
-cp *.py $PREFIX/work/
-cd ../..
-rm -rf ecoPCR-0.8.0
+tar -xzf ecoPCR-0.8.0.tar.gz || exit $?
+cd ecoPCR-0.8.0/src || exit $?
+make -j4 || exit $?
+cp ecofind ecogrep ecoisundertaxon ecoPCR $PREFIX/bin/ || exit $?
+cd ../tools || exit $?
+cp *.py $PREFIX/work/ || exit $?
+cd ../.. || exit $?
+rm -rf ecoPCR-0.8.0 || exit $?
 # Install Claident
-wget -c https://github.com/astanabe/Claident/archive/v0.2.2017.05.22.tar.gz -O Claident-0.2.2017.05.22.tar.gz
-tar -xzf Claident-0.2.2017.05.22.tar.gz
-cd Claident-0.2.2017.05.22
-make PREFIX=$PREFIX
-make PREFIX=$PREFIX install
-cp $PREFIX/share/claident/.claident ~/.claident
-cd ..
-rm -rf Claident-0.2.2017.05.22
+wget -c https://github.com/astanabe/Claident/archive/v0.2.2017.05.22.tar.gz -O Claident-0.2.2017.05.22.tar.gz || exit $?
+tar -xzf Claident-0.2.2017.05.22.tar.gz || exit $?
+cd Claident-0.2.2017.05.22 || exit $?
+make PREFIX=$PREFIX || exit $?
+make PREFIX=$PREFIX install || exit $?
+cp $PREFIX/share/claident/.claident ~/.claident || exit $?
+cd .. || exit $?
+rm -rf Claident-0.2.2017.05.22 || exit $?
 # Install Phylogears2
-wget -c https://github.com/astanabe/Phylogears/archive/v2.0.2016.09.06.tar.gz -O Phylogears-2.0.2016.09.06.tar.gz
-tar -xzf Phylogears-2.0.2016.09.06.tar.gz
-cd Phylogears-2.0.2016.09.06
-make PREFIX=$PREFIX
-make PREFIX=$PREFIX install
-cd ..
-rm -rf Phylogears-2.0.2016.09.06
+wget -c https://github.com/astanabe/Phylogears/archive/v2.0.2016.09.06.tar.gz -O Phylogears-2.0.2016.09.06.tar.gz || exit $?
+tar -xzf Phylogears-2.0.2016.09.06.tar.gz || exit $?
+cd Phylogears-2.0.2016.09.06 || exit $?
+make PREFIX=$PREFIX || exit $?
+make PREFIX=$PREFIX install || exit $?
+cd .. || exit $?
+rm -rf Phylogears-2.0.2016.09.06 || exit $?
 # Install MAFFT
-wget -c http://mafft.cbrc.jp/alignment/software/mafft-7.310-without-extensions-src.tgz
-tar -xzf mafft-7.310-without-extensions-src.tgz
-cd mafft-7.310-without-extensions/core
-perl -i -npe "s/^PREFIX = \\S+/PREFIX = \$ENV{\"PREFIX\"}/" Makefile
-make -j4
-make install
-cd ../..
-rm -rf mafft-7.310-without-extensions
+wget -c http://mafft.cbrc.jp/alignment/software/mafft-7.310-without-extensions-src.tgz || exit $?
+tar -xzf mafft-7.310-without-extensions-src.tgz || exit $?
+cd mafft-7.310-without-extensions/core || exit $?
+perl -i -npe "s/^PREFIX = \\S+/PREFIX = \$ENV{\"PREFIX\"}/" Makefile || exit $?
+make -j4 || exit $?
+make install || exit $?
+cd ../.. || exit $?
+rm -rf mafft-7.310-without-extensions || exit $?
 # Install Primer3
-wget -c -O primer3-2.3.7.tar.gz https://sourceforge.net/projects/primer3/files/primer3/2.3.7/primer3-2.3.7.tar.gz/download
-tar -xzf primer3-2.3.7.tar.gz
-cd primer3-2.3.7/src
-perl -i -npe "s/\\/opt\\/primer3_config/\$ENV{\"PREFIX\"}\\/bin\\/primer3_config/" primer3_boulder_main.c
-perl -i -npe "s/\\/opt\\/primer3_config/\$ENV{\"PREFIX\"}\\/bin\\/primer3_config/" thal_main.c
-make -j4
-cp long_seq_tm_test ntdpal ntthal oligotm primer3_core $PREFIX/bin/
-cp -R primer3_config $PREFIX/bin/
-cd ../..
-rm -rf primer3-2.3.7
+wget -c -O primer3-2.3.7.tar.gz https://sourceforge.net/projects/primer3/files/primer3/2.3.7/primer3-2.3.7.tar.gz/download || exit $?
+tar -xzf primer3-2.3.7.tar.gz || exit $?
+cd primer3-2.3.7/src || exit $?
+perl -i -npe "s/\\/opt\\/primer3_config/\$ENV{\"PREFIX\"}\\/bin\\/primer3_config/" primer3_boulder_main.c || exit $?
+perl -i -npe "s/\\/opt\\/primer3_config/\$ENV{\"PREFIX\"}\\/bin\\/primer3_config/" thal_main.c || exit $?
+make -j4 || exit $?
+cp long_seq_tm_test ntdpal ntthal oligotm primer3_core $PREFIX/bin/ || exit $?
+cp -R primer3_config $PREFIX/bin/ || exit $?
+cd ../.. || exit $?
+rm -rf primer3-2.3.7 || exit $?
 # Install VSEARCH
-wget -c https://github.com/torognes/vsearch/releases/download/v2.4.3/vsearch-2.4.3-linux-x86_64.tar.gz
-tar -xzf vsearch-2.4.3-linux-x86_64.tar.gz
-cd vsearch-2.4.3-linux-x86_64/bin
-cp vsearch $PREFIX/bin/
-cd ../..
-rm -rf vsearch-2.4.3-linux-x86_64
+wget -c https://github.com/torognes/vsearch/releases/download/v2.4.3/vsearch-2.4.3-linux-x86_64.tar.gz || exit $?
+tar -xzf vsearch-2.4.3-linux-x86_64.tar.gz || exit $?
+cd vsearch-2.4.3-linux-x86_64/bin || exit $?
+cp vsearch $PREFIX/bin/ || exit $?
+cd ../.. || exit $?
+rm -rf vsearch-2.4.3-linux-x86_64 || exit $?
 # Install EMBOSS
-wget -c ftp://emboss.open-bio.org/pub/EMBOSS/EMBOSS-6.6.0.tar.gz
-tar -xzf EMBOSS-6.6.0.tar.gz
-cd EMBOSS-6.6.0
-./configure --prefix=$PREFIX --without-x --without-java --disable-shared --disable-static
-make -j4
-make install
-cd ..
-rm -rf EMBOSS-6.6.0
+wget -c ftp://emboss.open-bio.org/pub/EMBOSS/EMBOSS-6.6.0.tar.gz || exit $?
+tar -xzf EMBOSS-6.6.0.tar.gz || exit $?
+cd EMBOSS-6.6.0 || exit $?
+./configure --prefix=$PREFIX --without-x --without-java --disable-shared --disable-static || exit $?
+make -j4 || exit $?
+make install || exit $?
+cd .. || exit $?
+rm -rf EMBOSS-6.6.0 || exit $?
 # Install NCBI BLAST+
-wget -c ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.6.0/ncbi-blast-2.6.0+-x64-linux.tar.gz
-tar -xzf ncbi-blast-2.6.0+-x64-linux.tar.gz
-cd ncbi-blast-2.6.0+/bin
-cp * $PREFIX/bin/
-cd ../..
-rm -rf ncbi-blast-2.6.0+
+wget -c ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.6.0/ncbi-blast-2.6.0+-x64-linux.tar.gz || exit $?
+tar -xzf ncbi-blast-2.6.0+-x64-linux.tar.gz || exit $?
+cd ncbi-blast-2.6.0+/bin || exit $?
+cp * $PREFIX/bin/ || exit $?
+cd ../.. || exit $?
+rm -rf ncbi-blast-2.6.0+ || exit $?
 # Install R
-wget -c https://cran.r-project.org/src/base/R-3/R-3.4.1.tar.gz
-tar -xzf R-3.4.1.tar.gz
-cd R-3.4.1
-./configure --prefix=$PREFIX --without-x
-make -j4
-make install
-cd ..
-rm -rf R-3.4.1
+wget -c https://cran.r-project.org/src/base/R-3/R-3.4.1.tar.gz || exit $?
+tar -xzf R-3.4.1.tar.gz || exit $?
+cd R-3.4.1 || exit $?
+./configure --prefix=$PREFIX --without-x || exit $?
+make -j4 || exit $?
+make install || exit $?
+cd .. || exit $?
+rm -rf R-3.4.1 || exit $?
